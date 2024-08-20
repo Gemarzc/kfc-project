e357c8b3c6a0bca01ea6afdce8646a8b73206d91
Gemarzc <gemacarret31@gmail.com>
2024-08-20 11:07:04 +0200

    fiejf

--- main.js
+++ main.js
@@ -21,3 +21,37 @@ const crearProducto = () => {
 }
 
 PUBLICAR$$.addEventListener('click', crearProducto)
+
+const pintarProductos = (listadoProductos) => {
+  const hamburguesas$$ = document.querySelector('.hamburguesas')
+  const wraps$$ = document.querySelector('.wraps')
+  const acompañantes$$ = document.querySelector('.acompañantes')
+  const bebidas$$ = document.querySelector('.bebidas')
+
+  hamburguesas$$.innerHTML = '<h2>Hamburguesas</h2>'
+  wraps$$.innerHTML = '<h2>Wraps</h2>'
+  acompañantes$$.innerHTML = '<h2>Acompañantes</h2>'
+  bebidas$$.innerHTML = '<h2>Bebidas</h2>'
+
+  for (const producto of listadoProductos) {
+    let productoHTML = `
+          <div class="producto">
+              <h3>${producto.nombre}</h3>
+              <div>
+                <img src="${producto.imagen}"/>
+              </div>
+              <p>${producto.precio}</p>
+          </div>
+      `
+
+    if (producto.tipo === 'hamburguesa') {
+      hamburguesas$$.innerHTML += productoHTML
+    } else if (producto.tipo === 'wrap') {
+      wraps$$.innerHTML += productoHTML
+    } else if (producto.tipo === 'acompañante') {
+      acompañantes$$.innerHTML += productoHTML
+    } else {
+      bebidas$$.innerHTML += productoHTML
+    }
+  }
+}
